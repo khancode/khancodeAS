d17c93b38d6f03e95e68880adfee89240f83c967
Omar Khan <okhan07@gatech.edu>
2015-06-28 03:26:10 -0400

    added CreateUser.java to communicate with restful api in creating a new user profile

--- Application/src/main/java/com/example/android/startup/CreateFragment.java
+++ Application/src/main/java/com/example/android/startup/CreateFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.android.restapi.CreateUser;
 import com.example.android.slidingtabscolors.R;
 
 /**
@@ -68,5 +69,17 @@ public class CreateFragment extends Fragment implements View.OnClickListener {
         Log.d(TAG, "password: " + password);
         Log.d(TAG, "confirmPassword: " + confirmPassword);
 
+        CreateUser asyncTask = new CreateUser(this);
+        asyncTask.execute(firstName, lastName, email, username, password);
+    }
+
+    public void receiveCreateUserResult(boolean success, String error)
+    {
+        final String TAG = "receiveCreateUserResult";
+
+        if (success)
+            Log.d(TAG, "Successfully created user! :D");
+        else
+            Log.d(TAG, "Failed to create user. error: " + error);
     }
 }
